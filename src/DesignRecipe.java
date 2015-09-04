a5e192dec9f224016101764ec03799ab8bee7a97
Thu Nguyen <nguyenth1@cs.grinnell.edu>
2015-09-04 15:37:34 -0500

    DesignRecipe.java

---
+++ src/DesignRecipe.java
@@ -0,0 +1,20 @@
+
+	public class DesignRecipe {
+	
+		public static String[] intersperse(String s, String[] arr){
+			if (s==null || arr==null){
+				return null;
+			}
+			else if (arr.length <2){
+				return arr;
+			}
+			else {
+			return null;
+		}
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	System.out.println (intersperse (null,null));
+		}
+	
+	}
